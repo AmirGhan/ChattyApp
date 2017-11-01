f86d1ee2db3b1fb244c457e3dc617385facacb9d
Amir <amir.ghandeharioon@gmail.com>
2017-11-01 16:27:03 +0000

    chatbar working

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,14 +1,38 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+  state = {};
+  constructor (props) {
+    super(props),
+    this.state = {
+      username: '',
+      content: ''
+    }
+  }
+
   render() {
+    console.log("Rendering <ChatBar/>");
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" placeholder="Your Name (Optional)"  onChange={this._handleUsernameChange} defaultValue={this.props.currentUser} />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={this._createMessage}/>
       </footer>
 
     );
   }
+
+
+  _handleUsernameChange = (event) => {
+    this.setState({username: event.target.value})
+  }
+
+
+  _createMessage = (event) => {
+    if (event.key === 'Enter') {
+    this.setState({content: event.target.value})
+    setTimeout(() => { this.props.messageCreated(this.state) }, 1);
+  }
+  }
+
 }
 export default ChatBar;
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,11 +3,17 @@ import Message from './Message.jsx';
 
 class MessageList extends Component {
   render() {
+    const {messages} = this.props;
+    const listMessages = messages.map((message) => {
+      return (
+      <Message key={message.id} username={message.username} content={message.content}/>
+    )
+  })
+    console.log("Rendering <MessageList/>");
     return (
       <main className="messages">
-
+      {listMessages}
         <div className="message system">
-          <Message />
         </div>
       </main>
     );
